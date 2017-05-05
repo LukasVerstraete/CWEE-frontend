c849e65159f6f87707d352fc845a83894a8eebf7
Lukas_Verstraete <lukas.verstraete@gmail.com>
2017-05-06 00:31:36 +0200

    created the card.js

--- js/game/card.js
+++ js/game/card.js
@@ -1,9 +1,29 @@
 /**
  * Created by lukas on 5-5-2017.
  */
-var card = function()
+function card()
 {
 
-    
+    var cardDescription;
+    var imageName;
+    var image;
 
+    var setCard = function(value, suite)
+    {
+        var stringValue = "" + value;
+        if(value < 10)
+        {
+            stringValue = "0" + stringValue;
+        }
+
+        cardDescription = value + " of " + suite;
+        imageName = "art/" + stringValue  + suite.charAt(0); + ".gif";
+        image = new image();
+        image.src = imageName;
+    };
+
+    return
+    {
+        setCard : setCard
+    };
 };
